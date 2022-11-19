dfe65a186df6674b162b3fb702d895e0eab171e3
tomin <tominotq@gmail.com>
2022-11-19 20:51:02 +0100

    vytvorenie modelu podla tabulky loginfo

--- App/Models/Loginfo.php
+++ App/Models/Loginfo.php
@@ -2,10 +2,77 @@
 
 namespace App\Models;
 
-class Loginfo
+use App\Core\Model;
+
+class Loginfo extends Model
 {
     public $id;
     public $email;
     public $nickname;
     public $password;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNickname()
+    {
+        return $this->nickname;
+    }
+
+    /**
+     * @param mixed $nickname
+     */
+    public function setNickname($nickname): void
+    {
+        $this->nickname = $nickname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
 }
