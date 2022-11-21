eb83ba1b078acc6bf7fbd4bf6a9d8f1323790ba4
tomin <tominotq@gmail.com>
2022-11-21 20:32:34 +0100

    overovanie vstupu na strane servera, test

--- App/Views/Auth/changepasswordpage.view.php
+++ App/Views/Auth/changepasswordpage.view.php
@@ -1,16 +1,15 @@
 <?php /** @var Array $data */ ?>
 
-<?php if ($data['error'] != "") { ?>
-    <div class="errortext">
-        <?= $data['error'] ?>
-    </div>
-<?php } ?>
-
 <!-- LOGIN section -->
 <div class="w3-container section" id="changepasswd">
     <h3 class="title"><i class="fa-solid fa-paw w3-jumbo"></i><br>Change password</h3>
+    <?php if ($data['error'] != "") { ?>
+        <div class="errortext">
+            <?= $data['error'] ?>
+        </div>
+    <?php } ?>
     <div>
-        <form class="contactform" method="post" action="?c=Auth&a=changepassword">
+        <form name="changepasswdForm" class="contactform" method="post" action="?c=Auth&a=changepassword">
             <label>
                 <input class="w3-input w3-border login-line" type="password" placeholder="New password" name="newpassword" required>
             </label>
@@ -18,7 +17,7 @@
                 <input class="w3-input w3-border login-line" type="password" placeholder="Repeat new password" name="repeatpassword" required>
             </label>
             <button class="button-menu mygrey-text login-button" type="submit">
-                    <i class="fa fa-paper-plane"></i>Change password
+                    <i class="fa fa-paper-plane"></i> Change password
             </button>
         </form>
     </div>
