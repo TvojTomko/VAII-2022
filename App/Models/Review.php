ac1dafc2a061e945ec475f8c6b2e8b7eead2effd
tomin <tominotq@gmail.com>
2023-01-31 21:57:45 +0100

    review -> model

---
+++ App/Models/Review.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use App\Core\Model;
+
+class Review extends Model
+{
+
+    protected $id;
+    protected $comment;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * @param mixed $comment
+     */
+    public function setComment($comment): void
+    {
+        $this->comment = $comment;
+    }
+}
